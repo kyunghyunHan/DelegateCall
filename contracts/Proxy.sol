27ee72eadbc59610ea057ef29181cf79c04e2187
kyunghyunHan <ekfqlwcjswl@gmail.com>
2022-05-25 12:52:24 +0900

    proxy

--- contracts/Proxy.sol
+++ contracts/Proxy.sol
@@ -14,5 +14,29 @@ contract Proxy {
 
   
 
+fallback()external{
+  assembly{
+    let ptr := mload(0x40)
+    calldatacop(ptr,0,calldatasize())
 
+    let result := dalegatecall(
+
+      gas(),
+      sload(implemetation.slot),
+      ptr,
+      calldatasize(),
+      0,
+      0
+    )
+    let size:= returndatasize()
+    returndatacopy(ptr,0,size)
+
+    switch result
+    case 0{
+      revert(ptr,size)
+    }
+    default{
+      return(ptr,size)
+    }
+}
 }
